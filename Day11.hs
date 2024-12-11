25d18ec72e7250bbd9900137abf64b5b38294e33
Joel Kaasinen <joel.kaasinen@gmail.com>
2024-12-11 09:08:56 +0200

    Day11 part2

--- Day11.hs
+++ Day11.hs
@@ -1,26 +1,62 @@
 module Day11 where
 
+import Control.Monad.Fix
 import Data.List
+import Numeric
+import Data.Array
 
 parse :: String -> [Int]
 parse = map read . words
 
 slurp = parse <$> readFile "input-11"
 
+countDigits :: Int -> Int
+countDigits 0 = 1
+countDigits n = go 0 n
+  where go d 0 = d
+        go d n = go (d+1) (div n 10)
+
+splitDigits :: Int -> Int -> (Int,Int)
+splitDigits digits n = divMod n (10^digits)
+
 step :: Int -> [Int]
 step 0 = [1]
 step n
-  | even len = [read $ take (div len 2) digits, read $ drop (div len 2) digits]
+  | even len = let (a,b) = splitDigits (div len 2) n in [a,b]
   | otherwise = [n*2024]
-  where digits = show n
-        len = length digits
+  where len = countDigits n
 
 stepAll = concatMap step
 
-part1 inp = length $ iterate stepAll inp !! 25
+naive n inp = length $ iterate stepAll inp !! n
 
-part2 inp = length $ iterate stepAll inp !! 75
+part1 inp = naive 25 inp
+
+lenAfter :: ((Int,Int) -> Int) -> (Int,Int) -> Int
+lenAfter f (0,_) = 1
+lenAfter f (s,0) = f ((s-1),1)
+lenAfter f (s,n)
+  | even digs = let (a,b) = splitDigits (div digs 2) n
+                in f (s-1, a) + f (s-1, b)
+  | otherwise = f (s-1, n*2024)
+  where digs = countDigits n
+
+lenAfterRecursive = fix lenAfter
+
+recursive n inp = sum $ map (\k -> lenAfterRecursive (n,k)) inp
+
+memoArray rng f = lookup
+  where arr = array rng [(i,f lookup i) | i <- range rng]
+        lookup i
+          | inRange rng i = arr ! i
+          | otherwise = f lookup i
+
+lenAfterMemo = memoArray ((0,0),(75,100000)) lenAfter
+
+memoized n inp = sum $ map (\k -> lenAfterMemo (n,k)) inp
+
+part2 inp = memoized 75 inp
 
 main = do
   --print . part1 =<< slurp
-  print . part2 =<< slurp -- Too slow, didn't complete in 5min even when compiled
+  print . part2 =<< slurp
