67dcdc62d07b2e851a091361d1a22bc8ae1f5f15
Joel Kaasinen <joel.kaasinen@metosin.fi>
2024-12-13 10:32:20 +0200

    Day13 part2

--- Day13.hs
+++ Day13.hs
@@ -2,8 +2,9 @@ module Day13 where
 
 import Data.List
 import Data.Maybe
+import Control.Monad
 
-type Pos = (Int,Int)
+type Pos = (Integer,Integer)
 
 parseCoords :: String -> Pos
 parseCoords c = read $ "(" ++ filter (`elem`"0123456789,") c ++ ")"
@@ -25,7 +26,7 @@ minMaybe Nothing y = y
 minMaybe x Nothing = x
 minMaybe (Just x) (Just y) = Just (min x y)
 
-solve1 :: (Pos,Pos,Pos) -> Int
+solve1 :: (Pos,Pos,Pos) -> Integer
 solve1 (a,b,goal) = case allRoutes2 a b goal
                     of [] -> 0
                        xs -> minimum [a*3+b | (a,b) <- xs]
@@ -43,13 +44,54 @@ allRoutes2 (ax,ay) (bx,by) (gx,gy) =
           , ry == 0
           , b == b' ]
 
+-- we know that m*a+n*b has only solutions of the form k*(gcd a b)
+soluble ((ax,ay),(bx,by),(gx,gy)) = mod gx (gcd ax bx) == 0 && mod gy (gcd ay by) == 0
 
-solve2 :: (Pos,Pos,Pos) -> Int
-solve2 (a,b,goal) = case allRoutes2 a b (goal +. offset)
-                    of [] -> 0
-                       xs -> minimum [a*3+b | (a,b) <- xs]
+-- no inputs are collinear! that means there is a unique solution
+collinear (ax,ay) (bx,by) = ax*by==ay*bx
 
-part2 = sum . map solve2
+aRoute (ax,ay) (bx,by) (gx,gy) = go 0
+    where go a = case (divMod (gx-a*ax) bx, divMod (gy-a*ay) by)
+                 of ((b,0),(b',0)) | b==b' -> (a,b)
+                    _ -> go (a+1)
+
+
+-- k*(ax,ay) + j*(bx,by) = (gx,gy)
+-- eli (gx,gy) - k*(ax,ay) = j*(bx,by)
+-- eli (gx-k*ax)/(gy-k*ay) = bx/by
+-- eli gx*by-k*ax*by = gy*bx-k*ay*bx
+-- eli k(ay*bx-ax*by) = gy*bx-gx*by
+-- eli k = (gy*bx-gx*by)/(ay*bx-ax*by)
+projectRoute (ax,ay) (bx,by) (gx,gy) = case divMod (gy*bx-gx*by) (ay*bx-ax*by)
+                                       of (a,0) -> Just (a, div (gx-a*ax) bx)
+                                          _ -> Nothing
+
+-- hack: when bx==by, projectRoute returns a false solution.
+-- check that flipping the arguments gives the same solution.
+projectRoute' a b g = do
+  (ka,kb) <- projectRoute a b g
+  (kb',ka') <- projectRoute b a g
+  guard (ka==ka')
+  guard (kb==kb')
+  return (ka,kb)
+
+doubleCheck (ax,ay) (bx,by) (gx,gy) = case projectRoute (ax,ay) (bx,by) (gx,gy)
+                                      of Just (a,b) -> Just (ck a b, a, b)
+                                         Nothing -> Nothing
+    where ck a b = a*ax + b*bx == gx && a*ay + b*by == gy
+
+solve2 :: (Pos,Pos,Pos) -> Integer
+solve2 (a,b,goal) = case projectRoute' a b goal
+                    of Nothing -> 0
+                       Just (a,b) -> a*3+b
+
+addOffset (a,b,g) = (a,b,g +. offset)
+
+part2 = sum . map (solve2 . addOffset)
 
 main = do
-  print . part1 =<< slurp
+  --print . part1 =<< slurp
+  print . part2 =<< slurp
+
+--(Just (False,48,129870129966),((74,12),(77,77),(10000000010944,10000000007968)))
+--(Just (False,81,344827586215),((81,11),(29,29),(10000000006822,10000000001152)))
