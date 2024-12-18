acfcef61b56277c09c78e5dad69844b871e08e64
Joel Kaasinen <joel.kaasinen@gmail.com>
2024-12-18 08:46:58 +0200

    polish Day18

--- Day18.hs
+++ Day18.hs
@@ -1,6 +1,5 @@
 module Day18 where
 
-import Debug.Trace
 import Data.Maybe
 import Data.List
 import qualified Data.Set as S
@@ -20,11 +19,12 @@ neighbours (x,y) = [(x-1,y)
                    ,(x,y-1)
                    ,(x,y+1)]
 
-bfs start end chart = go S.empty [(start,0,[start])]
-  where go visited ((p,d,path):qs)
+bfs :: Pos -> Pos -> Chart -> Maybe (Int,[Pos])
+bfs start end chart = go S.empty [(0,[start])]
+  where go visited ((d,path@(p:_)):qs)
           | p==end = Just (d,path)
           | S.member p visited = go visited qs
-          | otherwise = go (S.insert p visited) (qs ++ [(n,d+1,n:path) | n <- neighbours p
+          | otherwise = go (S.insert p visited) (qs ++ [(d+1,n:path) | n <- neighbours p
                                                                        , S.member n chart
                                                                        , not (S.member n visited)])
         go _ [] = Nothing
@@ -37,9 +37,9 @@ part2 inp = go allPositions initialPath inp
   where mkChart cs = S.difference allPositions $ S.fromList cs
         Just (_,initialPath) = bfs (0,0) (70,70) allPositions
         go chart path (i:inp)
-          | notElem i path = traceShow ("ignore",i) $ go chart' path inp
-          | otherwise = case traceShow ("delete",i,S.size chart') $ bfs (0,0) (70,70) chart'
-                        of Just (_,path') -> traceShow path $ go chart' path' inp
+          | notElem i path = go chart' path inp
+          | otherwise = case bfs (0,0) (70,70) chart'
+                        of Just (_,path') -> go chart' path' inp
                            Nothing -> i
           where chart' = S.delete i chart
 
