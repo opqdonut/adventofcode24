27c9eb50e8dca51e00c65030de5cae8263fc37c6
Joel Kaasinen <joel.kaasinen@gmail.com>
2025-01-10 14:40:03 +0200

    Day22 part2

--- Day22.hs
+++ Day22.hs
@@ -1,6 +1,9 @@
 module Day22 where
 
+import Data.List
 import Data.Bits
+import Data.Ord
+import qualified Data.Map as M
 
 mix :: Int -> Int -> Int
 mix = xor
@@ -20,5 +23,19 @@ slurp = map read . lines <$> readFile "input.22"
 
 part1 = sum . map secret2000
 
+prices init = map (`mod`10) $ take 2001 $ iterate secret init
+
+type Quad = (Int,Int,Int,Int)
+
+potentialSells :: Int -> M.Map Quad Int
+potentialSells init = M.fromListWith (\_ x -> x) $ go ds (tail ps)
+  where ps = prices init
+        ds = zipWith (-) (tail ps) ps
+        go (d1:d2:d3:d4:ds) (p1:p2:p3:p4:ps) = ((d1,d2,d3,d4),p4):go (d2:d3:d4:ds) (p2:p3:p4:ps)
+        go _ _ = []
+
+part2 inp = maximum $ M.elems $ foldl' (M.unionWith (+)) M.empty $ map potentialSells inp
+
 main = do
-  print . part1 =<< slurp
+  --print . part1 =<< slurp
+  print . part2 =<< slurp
