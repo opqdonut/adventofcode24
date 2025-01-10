eebdbd0b43b62212539036ae57575b438d04b75e
Joel Kaasinen <joel.kaasinen@gmail.com>
2025-01-10 20:55:07 +0200

    Day23

---
+++ Day23.hs
@@ -0,0 +1,33 @@
+module Day23 where
+
+import Data.List
+import qualified Data.Map as M
+
+type Graph = M.Map String [String]
+
+parse i = M.fromListWith (++) [e | (u,v) <- edges, e <- [(u,[v]),(v,[u])]]
+  where edges = map p . lines $ i
+        p s = let (a,'-':b) = break (=='-') s in (a,b)
+
+slurp = parse <$> readFile "input.23"
+example = parse <$> readFile "example.23"
+
+triangles g = [ [u,v,w] | (u,vs) <- M.assocs g, v <- vs, u<v, w <- g M.! v, v<w, u `elem` (g M.! w)]
+
+part1 g = length [t | t <- triangles g, any ("t" `isPrefixOf`) t]
+
+grow g clique = nub $ map (:clique) candidates
+  where candidates = filter lt $ foldr1 intersect $ map (g M.!) clique
+        lt x = x < head clique
+
+maxCliques g = go (triangles g)
+  where go cs = case concatMap (grow g) cs
+                of [] -> cs
+                   cs' -> go cs'
+
+part2 g = filter (not.(`elem`"[]\"")) $ show c
+  where c = head $ maxCliques g
+
+main = do
+  --print . part1 =<< slurp
+  putStrLn . part2 =<< slurp
