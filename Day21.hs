035b90573e03949f456b4afd20c688dfea05ad63
Joel Kaasinen <joel.kaasinen@gmail.com>
2025-01-10 14:04:27 +0200

    polish Day21

--- Day21.hs
+++ Day21.hs
@@ -1,9 +1,6 @@
 module Day21 where
 
 import Data.List
-import Data.Maybe
-import Data.Ord
-import Data.Array
 import Control.Monad
 import Control.Monad.State
 import qualified Data.Map as M
@@ -38,56 +35,6 @@ paths p from to
         vert = replicate (abs dY) (if dY < 0 then '^' else 'v')
         hori = replicate (abs dX) (if dX<0 then '<' else '>')
 
-pathsFor :: Pad -> [Char] -> [[Char]]
-pathsFor p s = map concat $ sequence [paths p a b | (a:b:_) <- tails ('A':s)]
-
-memoizedShortestArrowPadPath :: Char -> Char -> [Char]
-memoizedShortestArrowPadPath = \f t -> m M.! (f,t)
-  where m = M.fromList [((a,b),minimumBy (comparing length) (paths arrowpad a b)) | a <- "<>^vA", b <- "<>^vA"]
-
-greedyNumpadPath :: Pad -> [Char] -> [Char]
-greedyNumpadPath p s = concat $ [memoizedShortestArrowPadPath a b | (a:b:_) <- tails ('A':s)]
-
-unlookup v kvs = lookup v (map (\(a,b)->(b,a)) kvs)
-
-unbuttonsFor p ks = go (fromJust $ lookup 'A' p) ks
-  where go (x,y) ('<':ks) = go (x-1,y) ks
-        go (x,y) ('>':ks) = go (x+1,y) ks
-        go (x,y) ('v':ks) = go (x,y+1) ks
-        go (x,y) ('^':ks) = go (x,y-1) ks
-        go (x,y) ('A':ks) = fromJust (unlookup (x,y) p) : go (x,y) ks
-        go _ [] = []
-
-pathsFull code = concatMap (pathsFor arrowpad) . concatMap (pathsFor arrowpad) $ pathsFor numpad code
-
-allShortest ss = [s | s <- ss, length s == m]
-  where m = minimum (map length ss)
-
-bestPath = head . allShortest . pathsFull
-
-bestPath' code = head . allShortest . concatMap (pathsFor arrowpad) . allShortest . concatMap (pathsFor arrowpad) . allShortest $ pathsFor numpad code
-
-part1 inp = sum [length (bestPath code) * read (init code) | code <- inp]
-
-frequencies xs = [(length x, head x) | x <- group xs]
-
-shortestPaths2 0 code = (pathsFor numpad code)
-shortestPaths2 n code = map (greedyNumpadPath arrowpad) $ shortestPaths2 (n-1) code
-
-sP2 n code = minimum $ map length $ shortestPaths2 n code
-
---part2 inp = sum [sP2 25 code * read (init code) | code <- inp]
-
---                            <<^^A
---                         v<<AA>^AA>A
--- v<<A>>^AvA^A     <vA<AA>>^AAvA<^A>AAvA^A <vA>^AA<A>Av<<A>A>^AAAvA<^A>A
--- v<<A>>^AvA^A v<<A>>^AA<vA<A>>^AAvAA<^A>A <vA>^AA<A>Av<<A>A>^AAAvA<^A>A
---                         <AAv<AA>>^A
---                            ^^<<A
-
-
-------- fresh start
-
 type Memo = M.Map (Int,Char,Char) Int
 
 memo :: Ord a => (a -> b) -> a -> State (M.Map a b) b
@@ -112,4 +59,7 @@ search' limit from to = minimum <$> mapM (sumSteps (mgogo limit)) (paths numpad 
 
 search limit code = evalState (sumSteps (search' limit) code) M.empty
 
-part2 inp = sum [search 25 code * read (init code) | code <- inp]
+part lim inp = sum [search lim code * read (init code) | code <- inp]
+
+part1 = part 2
+part2 = part 25
